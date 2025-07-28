145c3cddfe94fb1409d1cdf8e1d1b91edb373e34
Chunzhou Yu <enjoylife.czzz@gmail.com>
2025-07-28 12:38:01 +0800

    add module-init

---
+++ module-init/main.go
@@ -0,0 +1,13 @@
+package main
+
+func init() {
+	println("module init function 1 is called ...")
+}
+
+func init() {
+	println("module init function 2 is called ...")
+}
+
+func main() {
+	println("main function is called ...")
+}
