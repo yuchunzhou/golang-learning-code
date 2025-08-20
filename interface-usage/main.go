2f22f83efcaf465a6b280f1828d9125cd43c2016
Chunzhou Yu <enjoylife.czzz@gmail.com>
2025-08-20 10:48:48 +0800

    Update

--- interface-usage/main.go
+++ interface-usage/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"reflect"
-	"unsafe"
 )
 
 type HelloInterface interface {
@@ -11,16 +10,7 @@ type HelloInterface interface {
 }
 
 func helloFunc(who HelloInterface) {
-	fmt.Printf("who ptr: %p\n", &who)
-
-	// do reflection
-	who_type := reflect.TypeOf(who)
-	if who_type.Kind() == reflect.Ptr {
-		fmt.Printf("who's type name: *%s\n", who_type.Elem().Name())
-	} else {
-		fmt.Printf("who's type name: %s\n", who_type.Name())
-	}
-
+	fmt.Printf("who's type: %s\n", reflect.TypeOf(who))
 	who.SayHello()
 }
 
@@ -28,7 +18,6 @@ type Foo struct{}
 
 func (foo Foo) SayHello() {
 	fmt.Println("hello from Foo")
-	fmt.Printf("foo ptr: %p\n", &foo)
 }
 
 type Bar struct{}
@@ -39,15 +28,7 @@ func (bar *Bar) SayHello() {
 
 func main() {
 	foo := Foo{}
-	// do reflection
-	foo_type := reflect.TypeOf(foo)
-	fmt.Println(foo_type.NumMethod()) // get exported methods’ count
-	for index := range foo_type.NumMethod() {
-		fmt.Printf("method name %d of %s: %s, address: %p\n", index, foo_type.Name(), foo_type.Method(index).Name, unsafe.Pointer(foo_type.Method(index).Func.Pointer()))
-	}
-
 	helloFunc(foo)
-	fmt.Printf("foo ptr: %p\n", &foo)
 
 	bar := Bar{}
 	helloFunc(&bar)
