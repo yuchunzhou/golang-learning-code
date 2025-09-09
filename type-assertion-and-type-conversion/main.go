c858755ffe535f1fdb8bc3b4c0ba888d4b235122
yuchunzhou.vendor <yuchunzhou.vendor@sensetime.com>
2025-09-09 10:39:27 +0800

    update

--- type-assertion-and-type-conversion/main.go
+++ type-assertion-and-type-conversion/main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+type Foo interface {
+	foo()
+}
+
+type Bar interface {
+	bar()
+}
+
+type Struct struct {
+}
+
+func (s *Struct) foo() {
+	fmt.Println("foo")
+}
+
+func (s *Struct) bar() {
+	fmt.Println("bar")
+}
+
+func foo(arg Foo) {
+	arg.(Bar).bar()
+}
+
 func main() {
 	// type assertion
 	var s1 interface{} = "hello"
@@ -11,6 +34,9 @@ func main() {
 	var s3 any
 	s4, ok := s3.(string)
 	fmt.Printf("s4: %s ok: %t\n", s4, ok)
+
+	s := &Struct{}
+	foo(s)
 
 	// type conversion
 	var a int16 = 0x1234
